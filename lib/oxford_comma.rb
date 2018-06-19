70cfb14de710704620190856cbaf1db520a9fb1f
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 00:51:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    array.join(", ").pop
+    array.push(", and #{array[-1}")
+    array
+  else 
   array.join(", ")
 end
