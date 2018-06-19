5fba8b9bb1915b9812e485b0e86cc4b37a9c8545
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 00:55:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array.join(", ").pop
-    array.push(", and #{array[-1}")
+    element_3 = array.pop
+    array.join(", ")
+    array.push(", and #{element_3}")
     array
   else 
   array.join(", ")
